c4919af18d4510b0f9342763f1ba822b647c9fa1
Kanika Agarwal <kanikaagarwal.ps12@gmail.com>
2024-05-18 12:16:44 +0530

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import './App.css'
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import {BrowserRouter, R} from 'react-router-dom';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import {Routes,Route} from "react-router-dom";
 import Home from './screens/Home';
 import BookStore from './screens/BookStore';
 import WTseller from './screens/WTseller';
@@ -11,14 +11,12 @@ function App() {
 
   return (
     <>
-      <Router>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/bookStore" element={<BookStore/>} />
           <Route path="/wtSeller" element={<WTseller/>} />
           <Route path="/testSeries" element={<TestSeries/>} />
         </Routes>
-      </Router>
     </>
   )
 }
